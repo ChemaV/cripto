b904a1a5d9b261c1fd587e576b72df42b73073ad
AleDataAnalyst <criptolmin@gmail.com>
2024-01-11 08:13:56 +0100

    creado componente NavBar con subcarpeta componente Links

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import LogoAllBlocksVCh from "@/components/LogoAllBlocksVCh"
+import Links from './links/Links'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div><LogoAllBlocksVCh /></div>
+    </div>
+    <div>
+    <Links />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,34 @@
+const Links = () => {
+
+  const links = [
+    {
+        title: 'Home',
+        path: '/',
+    },
+    {
+        title: 'Criptomonedas',
+        path: '/criptomonedas',
+    },
+    {
+        title: 'Intercambios',
+        path: '/intercambios',
+    },
+    {
+        title: 'Estadísticas',
+        path: '/estadisticas',
+    }
+    {
+        title: 'Contacto',
+        path: '/contacto',
+    }
+  ]
+  return (
+    <div>
+      {links.map((link=>(
+        <Link href={link.path} key={link.title}>{link.title}</Link>
+      )))}
+    </div>
+  )
+}
+
+export default Links
