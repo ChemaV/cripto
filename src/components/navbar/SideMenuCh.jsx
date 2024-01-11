684bb3f490cf8f371e58fa9881074d737ad12340
AleDataAnalyst <criptolmin@gmail.com>
2024-01-11 08:14:41 +0100

    Se cambió palabra intercambios

---
+++ src/components/navbar/SideMenuCh.jsx
@@ -0,0 +1,35 @@
+import { NextPage } from 'next';
+import { Box, VStack, Text, Image } from '@chakra-ui/react';
+import LogoAllBlocksVCh from '/LogoAllBlocksVCh';
+
+const SideMenuCh: NextPage = () => {
+  return (
+    <VStack spacing={4} align="center">
+      <Box>
+        <Text>Contacto</Text>
+        <Image src="/menuicons/icocontacto.svg" alt="Contacto" boxSize="30px" />
+      </Box>
+      <Box>
+        <Text>Noticias</Text>
+        <Image src="/menuicons/iconoticias.svg" alt="Noticias" boxSize="30px" />
+      </Box>
+      <Box>
+        <Text>Estadísticas</Text>
+        <Image src="/menuicons/icostats.svg" alt="Estadísticas" boxSize="30px" />
+      </Box>
+      <Box>
+        <Text>Intercambios</Text>
+        <Image src="/menuicons/icointercambios.svg" alt="Intercambios" boxSize="30px" />
+      </Box>
+      <Box>
+        <Text>Criptomonedas</Text>
+        <Image src="/menuicons/icocriptomonedas.svg" alt="Criptomonedas" boxSize="30px" />
+      </Box>
+      <Box>
+        <LogoAllBlocksVCh />
+      </Box>
+    </VStack>
+  )
+}
+
+export default SideMenuCh
