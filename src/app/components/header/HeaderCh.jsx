6890428d306a9cdbe48f5d4e72e3dcffa9117a01
AleDataAnalyst <criptolmin@gmail.com>
2024-01-13 00:02:00 +0100

    Estructura simplificada con estilos chakraui

--- src/app/components/header/HeaderCh.jsx
+++ src/app/components/header/HeaderCh.jsx
@@ -1,21 +1,15 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import CriptomonedasH1 from "./CriptomonedasH1";
 import CriptomonedasH2 from "./CriptomonedasH2";
 
 export default function HeaderCh() {
   return (
-    <Box position="relative" w="full" h="300px" overflow="hidden" textAlign="left" fontSize="36px" color="gray.700" fontFamily="'Chakra Petch', sans-serif">
-      {
-        <Box position="absolute" top="45px" left="58px" w="1225px" h="210px">
-        <Box position="absolute" top="0" left="0" borderRadius="14px" bg="gray.100" w="1225px" h="210px" />
-        <Box position="absolute" top="92px" left="64px" lineHeight="48px" fontWeight="medium" w="1121px" h="84px">
-          <CriptomonedasH2 />
-        </Box>
-        <Text position="absolute" top="19.4px" left="64px" fontSize="48px" fontWeight="semibold" color="purple.700" w="823px" h="65.1px">
-          <CriptomonedasH1 />
-        </Text>
-        </Box>
-      }
+    <Box boxShadow='xs' px='58px' py='24px' bg='gray.200' w="full" h="300px" overflow="hidden" >
+      <Flex className='header' direction="column" alignItems="start" justifyContent="center" wrap="wrap" gap='1' rounded='sm' bg="gray.300" h="auto" maxW="1225px" maxH="210px"  px='40px' py='20px' >
+        <CriptomonedasH1 />
+        <CriptomonedasH2 />
+      </Flex>
+          
     </Box>
   );
 };
