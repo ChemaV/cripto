5952b4f0f297b6b73e5cdc8dba009594950a36aa
AleDataAnalyst <criptolmin@gmail.com>
2024-01-13 00:01:36 +0100

    agregadas clases headingh1 y heading h2 con Tailwind desde globals.css

--- src/app/components/header/CriptomonedasH1.jsx
+++ src/app/components/header/CriptomonedasH1.jsx
@@ -2,6 +2,6 @@ import { Heading } from "@chakra-ui/react";
 
 export default function CriptomonedasH1() {
   return (
-    <Heading as='h1' size='lg' noOfLines={2} m="0">Criptomonedas</Heading>
+    <Heading className="headingh1" as='h1' size='md' noOfLines={2} m="0" color="purple.600"  fontSize="48px" fontWeight="semibold">Criptomonedas</Heading>
   );
 }
--- src/app/components/header/CriptomonedasH2.jsx
+++ src/app/components/header/CriptomonedasH2.jsx
@@ -2,6 +2,6 @@ import { Heading } from "@chakra-ui/react";
 
 export default function CriptomonedasH2() {
   return (
-    <Heading as='h2' size='md' noOfLines={2} m="0">Explora Todas las Criptomonedas en un Solo Lugar</Heading>
+    <Heading className="headingh2" as='h2' size='md' noOfLines={2} m="0">Explora Todas las Criptomonedas en un Solo Lugar</Heading>
   );
 }
