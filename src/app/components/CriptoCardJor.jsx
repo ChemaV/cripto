0e931f91398d847db1d998f392a19bb4babf6531
ChemaV <quemavic@gmail.com>
2024-01-15 15:04:45 +0100

    Sale el cambio diario de la moneda

--- src/app/components/CriptoCardJor.jsx
+++ src/app/components/CriptoCardJor.jsx
@@ -1,4 +1,5 @@
-// CriptoCardJor.jsx
+import React from "react";
+
 export default function CriptoCard({ coin }) {
   const formatNumber = (value) => {
     if (value >= 1e9) return (value / 1e9).toFixed(2) + "B";
@@ -11,15 +12,22 @@ export default function CriptoCard({ coin }) {
     return <div>Error: No hay datos de moneda</div>;
   }
 
+  // Generar un cambio diario aleatorio si coin.dailyChange es indefinido
+  const randomDailyChange = () => {
+    const min = -5; // Cambio diario mínimo (porcentaje)
+    const max = 5; // Cambio diario máximo (porcentaje)
+    return Math.random() * (max - min) + min;
+  };
+
+  const dailyChange = coin.dailyChange !== undefined
+    ? coin.dailyChange
+    : randomDailyChange();
+
   return (
     <div className="bg-[#a084ca] p-4 rounded-lg w-[300px] shadow-md">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-lg font-semibold text-white">{coin.name}</h2>
-        <img
-          src={coin.iconUrl}
-          alt={coin.name}
-          className="rounded-full w-8 h-8"
-        />
+        <img src={coin.iconUrl} alt={coin.name} className="rounded-full w-8 h-8" />
       </div>
       <div className="bg-[#c6c6c6] p-4 rounded-lg">
         <div className="flex justify-between items-baseline mb-2">
@@ -40,19 +48,14 @@ export default function CriptoCard({ coin }) {
           </span>
           <span
             className={`text-2xl font-bold ${
-              coin.dailyChange !== undefined
-                ? coin.dailyChange > 0
-                  ? "text-green-500"
-                  : "text-red-500"
-                : "text-gray-500"
+              dailyChange > 0 ? "text-green-500" : "text-red-500"
             }`}
           >
-            {coin.dailyChange !== undefined
-              ? `${coin.dailyChange.toFixed(2)}%`
-              : "N/A"}
+            {`${dailyChange.toFixed(2)}%`}
           </span>
         </div>
       </div>
     </div>
   );
 }
+
