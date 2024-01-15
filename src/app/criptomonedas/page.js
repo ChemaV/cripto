a3c1eceac334353a68883d7c36da1c35b6c0085a
AleDataAnalyst <criptolmin@gmail.com>
2024-01-15 15:17:53 +0100

    Api cripto incluida a página Criptomonedas

--- src/app/criptomonedas/page.js
+++ src/app/criptomonedas/page.js
@@ -1,6 +1,11 @@
+import Apirapid from "./components/Apirapid.jsx";
+
 const CriptomonedasPage = () => {
     return (
         <section className="intro">Criptomonedas</section>
+        <section>
+            <Apirapid />
+        </section>
     )
   }
   
