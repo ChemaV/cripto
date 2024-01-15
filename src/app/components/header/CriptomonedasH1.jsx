f11f8eae5d1b8a2f6ca7f5db16717e6b3ccac0c3
AleDataAnalyst <criptolmin@gmail.com>
2024-01-15 13:13:36 +0100

    quitados nombres de archivo

--- src/app/components/header/CriptomonedasH1.jsx
+++
@@ -1,7 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-
-export default function CriptomonedasH1() {
-  return (
-    <Heading className="headingh1" as='h1' size='md' noOfLines={2} m="0" color="purple.600"  fontSize="48px" fontWeight="semibold">Criptomonedas</Heading>
-  );
-}
--- src/app/components/header/CriptomonedasH2.jsx
+++
@@ -1,7 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-
-export default function CriptomonedasH2() {
-  return (
-    <Heading className="headingh2" as='h2' size='md' noOfLines={2} m="0">Explora Todas las Criptomonedas en un Solo Lugar</Heading>
-  );
-}
