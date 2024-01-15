035b8e7bf381a7c53dc2e9226a2db890a8d034b3
AleDataAnalyst <criptolmin@gmail.com>
2024-01-15 13:15:22 +0100

    json de contenidos

---
+++ src/app/constants/index.js
@@ -0,0 +1,200 @@
+import { block, blockchain, cryptowallet, decentralized, distributed, exchange, ledger, link, network, transaction } from "../assets/blockchainicons";
+import {  } from "../assets/images";
+import { facebook, instagram, twitter } from "../assets/socialmediaicons";
+
+export const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#criptomonedas", label: "Criptomonedas" },
+    { href: "#exchange", label: "Exchange" },
+    { href: "#estadisticas", label: "Estadísticas" },
+    { href: "#noticias", label: "Noticias" },
+    { href: "#contacto", label: "Contacto" },
+];
+
+export const blockchainicons = [
+    {
+        icon: block,
+        name: "block",
+        alt: "block",
+    },
+    {
+        icon: blockchain,
+        name: "blockchain",
+        alt: "blockchain",
+    },
+    {
+        icon: cryptowallet,
+        name: "cryptowallet",
+        alt: "cryptowallet",
+    },
+    {
+        icon: decentralized,
+        name: "decentralized",
+        alt: "decentralized",
+    },
+    {
+        icon: distributed,
+        name: "distributed",
+        alt: "distributed",
+    },
+    {
+        icon: exchange,
+        name: "exchange",
+        alt: "exchange",
+    },
+    {
+        icon: ledger,
+        name: "ledger",
+        alt: "ledger",
+    },
+    {
+        icon: link,
+        name: "link",
+        alt: "link",
+    },
+    {
+        icon: network,
+        name: "network",
+        alt: "network",
+    },
+    {
+        icon: transaction,
+        name: "transaction",
+        alt: "transaction",
+    },
+];
+
+export const topcriptos = [
+    {
+        thumbnail: thumbnailCripto1,
+        topCripto: topCripto1,
+    },
+    {
+        thumbnail: thumbnailCripto2,
+        topCripto: topCripto2,
+    },
+    {
+        thumbnail: thumbnailCripto3,
+        topCripto: topCripto3,
+    },
+];
+
+export const statistics = [
+    { value: '20k+', label: 'Criptomonedas' },
+    { value: '170+', label: 'Exchanges' },
+    { value: '30k+', label: 'Markets' },
+];
+
+export const popularcoins = [
+    {
+        imgURL: popularcoin1,
+        name: "Ethereum",
+        price: "$200.20",
+    },
+    {
+        imgURL: popularcoin2,
+        name: "Bitcoin",
+        price: "$210.20",
+    },
+    {
+        imgURL: popularcoin3,
+        name: "Nombre Popular Coin 3",
+        price: "$220.20",
+    },
+    {
+        imgURL: popularcoin4,
+        name: "Nombre Popular Coin 4",
+        price: "$230.20",
+    },
+];
+
+export const razones = [
+    {
+        razon1title: "Amplitud y Profundidad:",
+        razon1txt: "All Blocks abarca todos los aspectos del universo blockchain, desde las criptomonedas más populares hasta los proyectos más innovadores y disruptivos.",
+    },
+    {
+        razon2title: "Información Actualizada:",
+        razon2txt: "Mantenemos nuestras fuentes actualizadas constantemente, asegurándonos de que tengas acceso a la información más reciente y relevante.",
+    },
+    {
+        razon3title: "Información Actualizada:",
+        razon3txt: "Mantenemos nuestras fuentes actualizadas constantemente, asegurándonos de que tengas acceso a la información más reciente y relevante.",
+    },
+];
+
+export const pagetitles = [
+    {
+        name: homepage,
+        headingH1: "¡Bienvenido a All Blocks!",
+        headingH2: "Descubre el Universo Blockchain en All Blocks",
+        intro: ", tu ventana al fascinante mundo de las criptomonedas y las plataformas blockchain! En un universo en constante evolución, estamos aquí para ser tu guía definitiva, brindándote información precisa y actualizada sobre todos los mercados y tipos de plataformas blockchain.",
+        description: "En All Blocks, nos sumergimos en todos los rincones del cosmos blockchain, ofreciéndote una perspectiva completa de criptomonedas, exchanges, mercados y el emocionante mundo de los NFT. A diferencia de otras plataformas, nuestra misión es abarcar todos los aspectos de esta revolución financiera y tecnológica."
+    },
+    {
+        name: pagecripto,
+        headingH1: 'Criptomonedas',
+        headingH2: "Explora Todas las Criptomonedas en un Solo Lugar",
+        description: "En colaboración con la API Coinranking, te presentamos información detallada sobre todas las monedas, desde las icónicas Bitcoin y Ethereum hasta los tokens especializados como Basic Attention Token. En All Blocks, reconocemos que cada moneda es única, contribuyendo de manera significativa a la diversidad y amplitud del ecosistema blockchain."
+    },
+    {
+        name: pageexchange,
+        headingH1: 'Exchange',
+        headingH2: "Exchanges y Mercados: Tu Entrada al Mundo de las Transacciones",
+        description: "All Blocks no solo te proporciona datos sobre las criptomonedas, sino que también te lleva a la vanguardia de los exchanges y mercados más relevantes. Explora las plataformas líderes, conoce las tendencias del mercado y toma decisiones informadas para maximizar tus inversiones en este emocionante viaje financiero."
+    },
+    {
+        name: pagestats,
+        headingH1: 'Estadísticas',
+        headingH2: "Datos en Tiempo Real, Números que Hablan por Sí Mismos",
+        description: "Descubre un universo de información en nuestra página de estadísticas en tiempo real. Desde tasas de cambio hasta datos de mercado, te ofrecemos una visión profunda y actualizada del panorama criptográfico. Sumérgete en los números que dan forma al futuro financiero y toma decisiones informadas con All Blocks."
+    },
+    {
+        name: pagenoticias,
+        headingH1: 'Noticias',
+        headingH2: "Tu Entrada al Mundo de las Transacciones",
+        description: "Mantente informado con las últimas noticias y acontecimientos del mundo blockchain en nuestra página de noticias. Desde innovaciones tecnológicas hasta desarrollos regulatorios, te mantenemos al tanto de todo. Explora análisis expertos, entrevistas exclusivas y las tendencias más candentes en el fascinante universo de las criptomonedas y la tecnología blockchain. En All Blocks, la información es poder."
+    },
+    {
+        name: pagenft,
+        headingH1: 'NFT',
+        headingH2: "Sumérgete en el Universo NFT",
+        description: "Los NFT (Tokens No Fungibles) han revolucionado la manera en que entendemos y compartimos el arte digital, la música y mucho más. All Blocks te sumerge en este fascinante mundo, proporcionándote información clave sobre los NFT más destacados, las plataformas líderes y las últimas tendencias en este emocionante espacio."
+    }
+];
+
+
+export const footerLinks = [
+    {
+        title: "Servicios",
+        links: [
+            { name: "Criptomonedas", link: "#criptomonedas" },
+            { name: "Exchange", link: "#exchange" },
+            { name: "Estadísticas", link: "#estadisticas" },
+            { name: "Noticias", link: "#noticias" },
+        ],
+    },
+    {
+        title: "Ayuda",
+        links: [
+            { name: "Acerca de", link: "/" },
+            { name: "FAQs", link: "/" },
+            { name: "Cómo funciona", link: "/" },
+            { name: "Política de privacidad", link: "/" },
+            { name: "Política de pagos", link: "/" },
+        ],
+    },
+    {
+        title: "Contacta con All Blocks",
+        links: [
+            { name: "customer@allblocks.com", link: "mailto:customer@allblocks.com" },
+            { name: "+34000000", link: "tel:+34000000" },
+        ],
+    },
+];
+
+export const socialMedia = [
+    { src: facebook, alt: "facebook logo" },
+    { src: twitter, alt: "twitter logo" },
+    { src: instagram, alt: "instagram logo" },
+];
