d3daa67ba19e9ef11218468c127dd9e48288f39b
AleDataAnalyst <criptolmin@gmail.com>
2024-01-15 19:13:43 +0100

    textos rotados

--- src/app/components/navbar/NavbarLeft.jsx
+++ src/app/components/navbar/NavbarLeft.jsx
@@ -1,6 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
-import Logo from '/logoallblocks.svg';
+import Logo from '../../../../public/logoallblocks.svg';
 import { AiOutlineMenu, AiOutlineClose, AiOutlineInstagram, AiOutlineFacebook, AiOutlineTwitter } from 'react-icons/ai';
 import { useState } from 'react';
 
@@ -16,16 +18,14 @@ const NavbarLeft = () => {
       <nav className='fixed left-0 top-0 h-full w-[100px] bg-purple-600 shadow-md z-10 transition-width duration-500'>
         <div className='flex justify-between items-center h-full p-4'>
           <Link href="/">
-            <a>
               <Image
                 src={Logo}
                 alt="Logo All Blocks"
-                width="62"
-                height="141"
+                width="37"
+                height="100"
                 className="cursor-pointer"
                 priority
               />
-            </a>
           </Link>
           <div onClick={handleNav} className="cursor-pointer">
             {menuOpen ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
@@ -33,31 +33,41 @@ const NavbarLeft = () => {
         </div>
         <div className={
           menuOpen
-          ? "fixed left-0 top-0 w-[250px] h-full bg-[#f5f5f5] p-10 ease-in duration-500"
-          : "fixed left-[-100%] top-0 h-full p-10 ease-in duration-500"
+          ? "fixed left-0 top-0 w-[250px] h-full bg-purple.300 p-6 ease-in duration-500"
+          : "fixed left-[-100%] top-0 h-full p-6 ease-in duration-500"
         }>
-          <ul className="flex flex-col">
-            <li className='my-4 uppercase hover:border-b text-xl rotate-90'>
+          <Link href="/">
+              <Image
+                src={Logo}
+                alt="Logo All Blocks"
+                width="37"
+                height="100"
+                className="cursor-pointer pt-6"
+                priority
+              />
+          </Link>
+          <ul className="flex flex-col gap-12">
+            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 text-purple.600'>
               <Link href="/criptomonedas">
                 <span onClick={() => setMenuOpen(false)}>Criptomonedas</span>
               </Link>
             </li>
-            <li className='my-4 uppercase hover:border-b text-xl rotate-90'>
+            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 font-chakra text-purple.600'>
               <Link href="/exchange">
                 <span onClick={() => setMenuOpen(false)}>Exchange</span>
               </Link>
             </li>
-            <li className='my-4 uppercase hover:border-b text-xl rotate-90'>
+            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 font-chakra text-purple.600'>
               <Link href="/estadisticas">
                 <span onClick={() => setMenuOpen(false)}>Estadísticas</span>
               </Link>
             </li>
-            <li className='my-4 uppercase hover:border-b text-xl rotate-90'>
+            <li className='my-2 uppercase hover:border-b text-md -rotate-90 font-chakra text-purple.600'>
               <Link href="/noticias">
                 <span onClick={() => setMenuOpen(false)}>Noticias</span>
               </Link>
             </li>
-            <li className='my-4 uppercase hover:border-b text-xl rotate-90'>
+            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 font-chakra text-purple.600'>
               <Link href="/contacto">
                 <span onClick={() => setMenuOpen(false)}>Contacto</span>
               </Link>
@@ -68,18 +78,6 @@ const NavbarLeft = () => {
             <AiOutlineFacebook size={30} className="cursor-pointer" />
             <AiOutlineTwitter size={30} className="cursor-pointer" />
           </div>
-          <Link href="/">
-            <a>
-              <Image
-                src={Logo}
-                alt="Logo All Blocks"
-                width="62"
-                height="141"
-                className="cursor-pointer pt-6"
-                priority
-              />
-            </a>
-          </Link>
         </div>
       </nav>
       {menuOpen && (
