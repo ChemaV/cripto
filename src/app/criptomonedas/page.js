fd6c778c2ce6cce61edb02108ecbf80785388270
AleDataAnalyst <criptolmin@gmail.com>
2024-01-16 17:33:39 +0100

    Clases a nombres de marca

--- src/app/criptomonedas/page.js
+++ src/app/criptomonedas/page.js
@@ -5,10 +5,10 @@ const CriptomonedasPage = () => {
     <main className="w-full flex-start flex-col min-h-screenitems-start justify-between p-8">
       <section className="mb-5">
         <h3 className="introtxt font-chakra">
-          En colaboración con la API Coinranking, te presentamos información
+          En colaboración con la <span className="font-medium text-primary">API Coinranking</span>, te presentamos información
           detallada sobre todas las monedas, desde las icónicas Bitcoin y
           Ethereum hasta los tokens especializados como Basic Attention Token.
-          En All Blocks, reconocemos que cada moneda es única, contribuyendo de
+          En <span className="font-brand font-semibold text-primary">All Blocks</span>, reconocemos que cada moneda es única, contribuyendo de
           manera significativa a la diversidad y amplitud del ecosistema
           blockchain.
         </h3>
