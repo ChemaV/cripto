ab7544f42bb7cd1d760d30e3bd2440f61a67d55d
AleDataAnalyst <criptolmin@gmail.com>
2024-01-16 10:44:00 +0100

    Componente con chakraui

--- src/app/components/CardsgridflexCh.jsx
+++ src/app/components/CardsgridflexCh.jsx
@@ -1,8 +1,8 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { Flex, Wrap } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import Apirapid from './Apirapid';
 
-<Flex flexDirection='row' flexWrap={Wrap} minWidth='max-content' alignItems='center' gap='3'>
+<Flex flexDirection='row' minWidth='80%' alignItems='center' gap='3'>
     <Apirapid />
 </Flex>
