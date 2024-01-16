e784402ffba22e0d75671ca93bb7c772b44f223e
AleDataAnalyst <criptolmin@gmail.com>
2024-01-16 23:40:38 +0100

    Apertura con animación texto

--- src/app/components/navbar/NavbarLeft.jsx
+++ src/app/components/navbar/NavbarLeft.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
+import { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import Logo from '../../../../public/logoallblocks.svg';
 import { AiOutlineMenu, AiOutlineClose, AiOutlineInstagram, AiOutlineFacebook, AiOutlineTwitter } from 'react-icons/ai';
-import { useState } from 'react';
 
 const NavbarLeft = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -15,86 +15,61 @@ const NavbarLeft = () => {
 
   return (
     <>
-      <nav className='fixed left-0 top-0 h-full w-[100px] bg-purple-600 shadow-md z-10 transition-width duration-500'>
-        <div className='flex justify-between items-stretch h-full p-4'>
+      <nav className={`fixed left-0 top-0 h-full ${menuOpen ? 'w-[250px]' : 'w-[100px]'} py-2 bg-primary shadow-md z-10 transition-all duration-500`}>
+        <div className="flex justify-between items-center h-fit p-2 mb-8">
           <Link href="/">
-              <Image
-                src={Logo}
-                alt="Logo All Blocks"
-                width="37"
-                height="100"
-                className="cursor-pointer"
-                priority
-              />
+            <Image
+              src={Logo}
+              alt="Logo All Blocks"
+              width="37"
+              height="100"
+              className="cursor-pointer"
+              priority
+            />
           </Link>
           <div onClick={handleNav} className="cursor-pointer">
-            {menuOpen ? <AiOutlineClose size={25} className="text-white" /> : <AiOutlineMenu size={25} className="text-white" />}
+            {menuOpen ? (
+              <AiOutlineClose size={25} className="text-white" />
+            ) : (
+              <AiOutlineMenu size={25} className="text-white" />
+            )}
           </div>
         </div>
-        <div className={menuOpen ? "flex flex-col gap-4 items-stretch p-4" : "hidden"}>
-        <ul className="flex flex-col">
-            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 font-chakra text-white.600'>
+        <div className={`flex flex-col mt-10 mb-6 py-6 pl-6 ${menuOpen ? 'items-start' : 'items-center'}`}>
+          <ul className={`flex flex-col justify-between space-y-20 font-chakra transition-all duration-500`}>
+            <li className={`uppercase hover:text-purple-light hover:font-bold text-sm ${menuOpen ? 'rotate-0 text-xl text-white' : '-rotate-90 text-white'} transition-all duration-500`}>
               <Link href="/criptomonedas">
                 <span onClick={() => setMenuOpen(false)}>Criptomonedas</span>
               </Link>
             </li>
-            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 font-chakra text-white.600'>
+            <li className={`uppercase hover:text-purple-light hover:font-bold text-sm ${menuOpen ? 'rotate-0 text-xl text-white' : '-rotate-90 text-white'} transition-all duration-500`}>
               <Link href="/exchange">
                 <span onClick={() => setMenuOpen(false)}>Exchange</span>
               </Link>
             </li>
-            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 font-chakra text-white.600'>
+            <li className={`uppercase hover:text-purple-light hover:font-bold text-sm ${menuOpen ? 'rotate-0 text-xl text-white' : '-rotate-90 text-white'} transition-all duration-500`}>
               <Link href="/estadisticas">
                 <span onClick={() => setMenuOpen(false)}>Estadísticas</span>
               </Link>
             </li>
-            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 font-chakra text-white.600'>
+            <li className={`uppercase hover:text-purple-light hover:font-bold text-sm ${menuOpen ? 'rotate-0 text-xl text-white' : '-rotate-90 text-white'} transition-all duration-500`}>
               <Link href="/noticias">
                 <span onClick={() => setMenuOpen(false)}>Noticias</span>
               </Link>
             </li>
-            <li className='my-2 uppercase hover:border-b text-sm -rotate-90 font-chakra text-white.600'>
+            <li className={`uppercase hover:text-purple-light hover:font-bold text-sm ${menuOpen ? 'rotate-0 text-xl text-white' : '-rotate-90 text-white'} transition-all duration-500`}>
               <Link href="/contacto">
                 <span onClick={() => setMenuOpen(false)}>Contacto</span>
               </Link>
             </li>
           </ul>
+        </div>
+        <div className="flex flex-row justify-around pt-8 items-end">
+          <AiOutlineInstagram size={20} className="cursor-pointer text-white" />
+          <AiOutlineFacebook size={20} className="cursor-pointer text-white" />
+          <AiOutlineTwitter size={20} className="cursor-pointer text-white" />
         </div>
       </nav>
-      <div className={menuOpen ? "fixed left-110 top-0 w-[100px] h-full bg-purple-300 p-6 ease-in duration-500" : "hidden"}>
-          <ul className="flex flex-col gap-10">
-            <li className='my-5 uppercase hover:border-b text-sm -rotate-90 text-purple-600'>
-              <Link href="/criptomonedas">
-                <span onClick={() => setMenuOpen(false)}>Criptomonedas</span>
-              </Link>
-            </li>
-            <li className='my-5 uppercase hover:border-b text-sm -rotate-90 font-chakra text-purple-600'>
-              <Link href="/exchange">
-                <span onClick={() => setMenuOpen(false)}>Exchange</span>
-              </Link>
-            </li>
-            <li className='my-5 uppercase hover:border-b text-sm -rotate-90 font-chakra text-purple-600'>
-              <Link href="/estadisticas">
-                <span onClick={() => setMenuOpen(false)}>Estadísticas</span>
-              </Link>
-            </li>
-            <li className='my-5 uppercase hover:border-b text-sm -rotate-90 font-chakra text-purple-600'>
-              <Link href="/noticias">
-                <span onClick={() => setMenuOpen(false)}>Noticias</span>
-              </Link>
-            </li>
-            <li className='my-5 uppercase hover:border-b text-sm -rotate-90 font-chakra text-purple-600'>
-              <Link href="/contacto">
-                <span onClick={() => setMenuOpen(false)}>Contacto</span>
-              </Link>
-            </li>
-          </ul>
-          <div className='flex flex-row justify-around pt-16 items-center'>
-            <AiOutlineInstagram size={30} className="cursor-pointer" bg-white />
-            <AiOutlineFacebook size={30} className="cursor-pointer" bg-white />
-            <AiOutlineTwitter size={30} className="cursor-pointer" bg-white />
-          </div>
-      </div>
     </>
   );
 };
