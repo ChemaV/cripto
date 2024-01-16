52e91edc713d0c667da6f2192c8eba6f435c067d
AleDataAnalyst <criptolmin@gmail.com>
2024-01-16 14:01:41 +0100

    Quitados textos correspondientes a header

--- src/app/criptomonedas/page.js
+++ src/app/criptomonedas/page.js
@@ -2,22 +2,20 @@ import Apirapid from "../components/Apirapid.jsx";
 
 const CriptomonedasPage = () => {
   return (
-    <main className="w-full flex-center flex-col min-h-screenitems-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <section className="hero">
-          <h1 className="headingh1">Criptomonedas</h1>
-          <h2>Explora Todas las Criptomonedas en un Solo Lugar</h2>
-        </section>
-        <section>
-          <h3 className="introtxt">
-          En colaboración con la API Coinranking, te presentamos información detallada sobre todas las monedas, desde las icónicas Bitcoin y Ethereum hasta los tokens especializados como Basic Attention Token. En All Blocks, reconocemos que cada moneda es única, contribuyendo de manera significativa a la diversidad y amplitud del ecosistema blockchain.
-          </h3>
-        </section>
-        <section className="intro">Criptomonedas</section>
-        <section>
-          <Apirapid />
-        </section>
-      </div>
+    <main className="w-full flex-start flex-col min-h-screenitems-start justify-between p-8">
+      <section className="mb-5">
+        <h3 className="introtxt font-chakra">
+          En colaboración con la API Coinranking, te presentamos información
+          detallada sobre todas las monedas, desde las icónicas Bitcoin y
+          Ethereum hasta los tokens especializados como Basic Attention Token.
+          En All Blocks, reconocemos que cada moneda es única, contribuyendo de
+          manera significativa a la diversidad y amplitud del ecosistema
+          blockchain.
+        </h3>
+      </section>
+      <section className="flex min-h-screen items-start justify-between p-0">
+        <Apirapid />
+      </section>
     </main>
   );
 };
