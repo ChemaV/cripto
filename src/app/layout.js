e99ab8dad331f59a75ad0062d8222eccd3b1f604
AleDataAnalyst <criptolmin@gmail.com>
2024-01-16 23:41:40 +0100

    footer position fixed pero no ok del todo

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
           <GridItem gridArea="main" bg="white" className="max-h-screen">
             {children}
           </GridItem>
-          <GridItem area="footer" alignSelf="end" position="sticky" bottom="0">
+          <GridItem area="footer" alignSelf="center" position="fixed" bottom="0" className="w-full left-100">
             <Footer />
           </GridItem>
         </Grid>
