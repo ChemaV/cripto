c932f37a38a724a0cc8c26d2ff58300f337f5233
AleDataAnalyst <criptolmin@gmail.com>
2024-01-17 00:32:09 +0100

    Valores Tailwind

--- src/app/components/header/CriptomonedasH1.jsx
+++ src/app/components/header/CriptomonedasH1.jsx
@@ -2,7 +2,7 @@ import { Heading } from "@chakra-ui/react";
 
 const CriptomonedasH1 = () => {
 return (
-    <Heading className="headingh1" as="h1" noOfLines={2} m="0">
+    <Heading className="font-chakra text-primary font-semibold text-4xl" as="h1" noOfLines={2} m="0">
       Criptomonedas
     </Heading>
   );
