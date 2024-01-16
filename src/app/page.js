7872787ac376af1a2ef8e0307bda7b268cd80a8b
AleDataAnalyst <criptolmin@gmail.com>
2024-01-16 11:52:08 +0100

    font-chakra aplicada a textos

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,17 @@ import Apirapid from "./components/Apirapid.jsx";
 
 export default function Home() {
   return (
-    <main className="w-full flex-center flex-col min-h-screenitems-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <section className="hero">
-          <h1 className="headingh1">¡Bienvenido a All Blocks!</h1>
-          <h2>Descubre el Universo Blockchain en All Blocks</h2>
+    <main className="w-full flex-start flex-col min-h-screenitems-start justify-between p-8">
+      <section className="hero">
+          <h1 className="headingh1 font-chakra">¡Bienvenido a All Blocks!</h1>
+          <h2 className="headingh2 font-chakra">Descubre el Universo Blockchain en All Blocks</h2>
         </section>
-        <section>
-          <h3 className="introtxt">Tu ventana al fascinante mundo de las criptomonedas y las plataformas blockchain! En un universo en constante evolución, estamos aquí para ser tu guía definitiva, brindándote información precisa y actualizada sobre todos los mercados y tipos de plataformas blockchain.</h3>
+        <section className="mb-5">
+          <h3 className="introtxt font-chakra">Tu ventana al fascinante mundo de las criptomonedas y las plataformas blockchain! En un universo en constante evolución, estamos aquí para ser tu guía definitiva, brindándote información precisa y actualizada sobre todos los mercados y tipos de plataformas blockchain.</h3>
         </section>
-        <section className="flex min-h-screen items-center justify-between p-20">  
+        <section className="flex min-h-screen items-start justify-between p-0">  
           <Apirapid />
         </section>
-      </div>
     </main>
   );
 }
