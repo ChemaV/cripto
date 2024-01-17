5d34b7e6289e6153cee71fd294783eafdbc85ebc
JREdesign <jr.eugercios@gmail.com>
2024-01-17 10:50:27 +0100

    api correcta, pero con errores

--- src/app/noticias/apinews.js
+++ src/app/noticias/apinews.js
@@ -1,30 +1,20 @@
+'use client';
 export async function fetchData() {
-  const url = 'https://cryptocurrency-news2.p.rapidapi.com/v1/theguardian';
-  const options = {
-    method: "GET",
+  const url = 'https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw';
+const options = {
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "b77c225c5dmsh4e1d62edcee39e3p168dfbjsn0d8babd40c6c",
-      "X-RapidAPI-Host": "cryptocurrency-news2.p.rapidapi.com",
-    },
-  };
+        'X-BingApis-SDK': 'true',
+        'X-RapidAPI-Key': '2e3659d649mshbd6eae1fc8d04f0p156fc4jsn324a5b3d8bd5',
+        'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+    }
+};
 
-  try {
+try {
     const response = await fetch(url, options);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const result = await response.json();
-
-    // Asegúrate de que result.data es una matriz
-    if (Array.isArray(result.data)) {
-      return result.data;
-    } else {
-      throw new Error("Datos de formato incorrecto recibidos de la API");
-    }
-  } catch (error) {
-    console.error("Error fetching data from the API:", error.message);
-    throw new Error("Error fetching data from the API");
-  }
+    const result = await response.text();
+    console.log(result);
+} catch (error) {
+    console.error(error);
+}
 }
