bd3c04bf403915e9f832b915b9fd1b4a0dab72b3
JREdesign <jr.eugercios@gmail.com>
2024-01-17 10:07:26 +0100

    cambio de color links

--- src/app/exchange/CriptoExchanges.jsx
+++ src/app/exchange/CriptoExchanges.jsx
@@ -24,7 +24,7 @@ const CriptoExchanges = () => {
             <p className="text-sm">{exchange.descripcion}</p>
           </div>
           <div className="items-center p-6 flex justify-end">
-            <button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
+            <button className="inline-flex text-white items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary h-10 px-4 py-2">
               <a className="underline" href="#">
                 Visitar Sitio
               </a>
