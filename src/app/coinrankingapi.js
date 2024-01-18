078d54d7b2b1261527910d5e9be9f1cd5db187a1
AleDataAnalyst <criptolmin@gmail.com>
2024-01-18 16:51:14 +0100

    Intenté pasar api key a .process.env pero se mantiene visible

---
+++ src/app/coinrankingapi.js
@@ -0,0 +1,20 @@
+export async function fetchData() {
+  const url =
+    "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "96278de74dmsh2cc95c5ad7479fep1f02efjsn71eb14e7a5d4",
+      "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error al obtener datos de la API");
+  }
+}
