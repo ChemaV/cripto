f78fd8caab37e64fb7e9b1e21deff9dd466df2c5
AleDataAnalyst <criptolmin@gmail.com>
2024-01-18 10:59:53 +0100

    Tamño texto responsive

--- src/app/components/header/NoticiasH2.jsx
+++ src/app/components/header/NoticiasH2.jsx
@@ -2,8 +2,8 @@ import { Heading } from "@chakra-ui/react";
 
 const NoticiasH2 = () => {
 return (
-    <Heading className="headingh2" as="h2" noOfLines={2} m="0">
-      Al Día con Blockchain: Novedades, Análisis y Tendencias
+  <Heading className="font-chakra text-gray-title font-medium text-md lg:text-2xl xl:text-3xl" as="h2" noOfLines={3} m="0">
+  Al Día con Blockchain: Novedades, Análisis y Tendencias
     </Heading>
   );
 };
