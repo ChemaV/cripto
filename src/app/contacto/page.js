6df734bd33683631908393f79f50b193f0f79bc1
AleDataAnalyst <criptolmin@gmail.com>
2024-01-18 10:37:01 +0100

    ContactoPage

--- src/app/contacto/page.js
+++ src/app/contacto/page.js
@@ -1,11 +1,15 @@
-import Contacto from '../contacto/Contacto.jsx'
+import HeaderContacto from "../components/header/HeaderContacto.jsx";
+import ContactoForm from "../components/ContactoForm.jsx";
 
-const ContactoPAge = () => {
-    return (
-      <Contacto />
-    )
-  }
-  
-export default Contacto
-  
+const ContactoPage = () => {
+  return (
+    <main className="w-full flex-start flex-col min-h-screenitems-start justify-between p-8">
+      <HeaderContacto />
+      <section className="flex min-h-screen items-start justify-center md:justify-between p-0">
+        <ContactoForm />
+      </section>
+    </main>
+  );
+};
 
+export default ContactoPage;
